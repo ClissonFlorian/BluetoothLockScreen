2bffa73ecea92956922f026c07b702c9b39e3cb3
ClissonFlorian <florian.clisson@gmail.com>
2018-03-18 14:08:22 +0100

    Bluetooth-LockScreen
    Lock Windows Session when bluetooth uid defined is not found

---
+++ Bluetooth-LockScreen.ps1
@@ -0,0 +1,15 @@
+﻿
+#"Bluetooth" One plus 5
+
+$uid="94:65:2D:7C:0B:EE"
+
+$isFound = @(btdiscovery -b "94:65:2D:7C:0B:EE").Count
+
+
+if($isFound -ne 1){
+
+
+   rundll32.exe user32.dll,LockWorkStation
+
+
+}
